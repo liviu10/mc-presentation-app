14a1b2cf2368a548d456b53b6ed46cdbaa08951e
Liviu Voica <voica.liviu10@gmail.com>
2021-09-08 07:55:16 +0300

    Schedule Appointment Page Improvements: - created the VueJs template for the Schedule Appointment page; - added a button that refers to the questionnaire; - create a route URL to the questionnarie page;

---
+++ resources/js/pages/user/schedule-appointment/start-questionnaire/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12 m-auto">
+      <h1>APP START QUESTIONNAIRE PAGE</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartQuestionnairePage'
+}
+</script>
