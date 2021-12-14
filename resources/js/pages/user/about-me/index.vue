00fa527b44c4cbb416b66c154d19285bcea33879
Liviu Voica <voica.liviu10@gmail.com>
2021-12-14 07:06:53 +0200

    Design improvments: - refactor the CSS and HTML structure for the About me page and all of it's components;

--- resources/js/pages/user/about-me/index.vue
+++ resources/js/pages/user/about-me/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row">
     <div class="col-lg-12 m-auto">
-      <div class="container lv-con-pg-about">
+      <div class="lv-pg-about-me">
         <!-- ABOUT ME VIDEO, SECTION START -->
-        <div class="lv-con-pg-about-video">
+        <div class="lv-pg-about-me-video">
           <video controls>
             <source src="/videos/demo_video.mp4" type="video/mp4">
           </video>
@@ -21,7 +21,7 @@
 import AboutMeTimeline from '~/pages/user/about-me/partials/AboutMeTimeline.vue'
 
 export default {
-  name: 'AboutMePage',
+  name: 'AboutMe',
   components: {
     AboutMeTimeline
   },
