8e93b23ca0977881882e8210edea8519705cf04a
Liviu Voica <voica.liviu10@gmail.com>
2022-01-07 15:22:59 +0200

    Blog system improvements: - small typo in color name for $tetradic_pale_cerulean variable from the current color theme; - refactor the CSS styles, HTML structure and Javascript functions for "/blog/article", "/blog/audio" and "/blog/video" pages; - added new methods in blog.js to handle the api requests; - removed unused javascript from MainCategories.vue component; - changed the translations (romanian and english) for "/blog/article", "/blog/audio" and "/blog/video" page title; - included new fields in the BlogSubcategoryController (methods: getAllWrittenBlogArticles, getAllAudioBlogArticles and getAllVideoBlogArticles); - refactor BlogArticlesSeeder, BlogArticleCommentsSeeder and BlogArticleCommentRepliesSeeder;

--- resources/js/pages/user/blog/partials/WrittenListArticles.vue
+++ resources/js/pages/user/blog/partials/WrittenListArticles.vue
@@ -7,40 +7,50 @@
     <div v-if="!displayAllWrittenBlogArticles" class="lv-pg-articles-body">
       <h1>{{ notifyMessage }}</h1>
     </div>
-    <div v-for="blogContent in displayAllWrittenBlogArticles" v-else
-         :key="blogContent.id"
-         class="lv-pg-articles-body"
-    >
-      <div v-for="writtenArticle in blogContent.blog_articles" :key="writtenArticle.id">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">
-              <a :href="writtenArticle.blog_article_path + '/' + writtenArticle.id">
-                {{ writtenArticle.blog_article_title }}
+    <div v-else>
+      <div v-for="blogContent in displayAllWrittenBlogArticles"
+           :key="blogContent.id"
+           class="lv-pg-articles-body"
+      >
+        <div v-for="writtenArticle in blogContent.blog_articles" :key="writtenArticle.id" class="my-3">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title">
+                <a :href="writtenArticle.blog_article_path + '/' + writtenArticle.id">
+                  <span>{{ writtenArticle.blog_article_title }}</span>
+                </a>
+                <span v-if="writtenArticle.blog_article_time <= 1">
+                  ({{ $t('user.blog_system_pages.written_article_blog_pages.reading_time.less_than_one_minute') }})
+                </span>
+                <span v-else>
+                  ({{ writtenArticle.blog_article_time }} {{ $t('user.blog_system_pages.written_article_blog_pages.reading_time.more_than_one_minute') }})
+                </span>
+                <p>
+                  {{ $t('user.blog_system_pages.subcategory_name') }}
+                  <span>
+                    <a href="">{{ blogContent.blog_subcategory_title }}</a>
+                  </span>
+                </p>
+                <p v-if="writtenArticle.updated_at == writtenArticle.created_at">
+                  {{ $t('user.blog_system_pages.general_settings.published_on') }}
+                  <span>{{ new Date(writtenArticle.created_at).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' }) }}</span>
+                </p>
+                <p v-else>
+                  {{ $t('user.blog_system_pages.general_settings.modified_on') }}
+                  <span>{{ new Date(writtenArticle.updated_at).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' }) }}</span>
+                </p>
+              </h3>
+              <p class="card-text">
+                <fa icon="quote-left" fixed-width />
+                {{ writtenArticle.blog_article_short_description }}
+              </p>
+            </div>
+            <div class="card-body">
+              <a :href="writtenArticle.blog_article_path + '/' + writtenArticle.id" class="btn btn-primary">
+                <fa icon="book-reader" fixed-width />
+                {{ $t('user.blog_system_pages.written_article_blog_pages.read_more') }}
               </a>
-              <span v-if="writtenArticle.blog_article_time <= 1">
-                ({{ $t('user.blog_system_pages.written_article_blog_pages.reading_time.less_than_one_minute') }})
-              </span>
-              <span v-else>
-                ({{ writtenArticle.blog_article_time }} {{ $t('user.blog_system_pages.written_article_blog_pages.reading_time.more_than_one_minute') }})
-              </span>
-              <p v-if="writtenArticle.updated_at == writtenArticle.created_at">
-                {{ $t('user.blog_system_pages.general_settings.published_on') }}
-                <span>{{ new Date(writtenArticle.created_at).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' }) }}</span>
-              </p>
-              <p v-else-if="writtenArticle.updated_at > writtenArticle.created_at">
-                {{ $t('user.blog_system_pages.general_settings.modified_on') }}
-                <span>{{ new Date(writtenArticle.updated_at).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' }) }}</span>
-              </p>
-            </h5>
-            <p class="card-text">
-              <fa icon="quote-left" fixed-width />
-              {{ writtenArticle.blog_article_short_description }}
-            </p>
-            <a :href="writtenArticle.blog_article_path + '/' + writtenArticle.id" class="btn btn-primary">
-              <fa icon="book-reader" fixed-width />
-              {{ $t('user.blog_system_pages.written_article_blog_pages.read_more') }}
-            </a>
+            </div>
           </div>
         </div>
       </div>
@@ -61,7 +71,10 @@
 <script>
 import Vue from 'vue'
 import axios from 'axios'
+import Vuex, { mapGetters, mapActions } from 'vuex'
+
 Vue.use(axios)
+Vue.use(Vuex)
 
 window.axios = require('axios')
 
@@ -70,25 +83,28 @@ export default {
   data: function () {
     return {
       notifyCode: null,
-      notifyMessage: null,
-      displayAllWrittenBlogArticles: null
+      notifyMessage: null
     }
   },
-  mounted () {
-    this.getAllWrittenBlogArticles()
+  computed: {
+    ...mapGetters({
+      listOfWrittenArticles: 'blog/listOfWrittenArticles'
+    }),
+    displayAllWrittenBlogArticles () {
+      return this.listOfWrittenArticles.records
+    },
+    getHttpStatusResponseCode () {
+      // TODO: How to catch api endpoint errors and display them to the user
+      return this.listOfWrittenArticles.http_response_code
+    }
+  },
+  created () {
+    this.fetchListOfWrittenArticles()
   },
   methods: {
-    getAllWrittenBlogArticles: function () {
-      const url = window.location.origin
-      const apiEndPoint = '/api/blog/articles/all-written-articles'
-      const fullApiUrl = url + apiEndPoint
-      axios
-        .get(fullApiUrl)
-        .then(response => {
-          console.log('>>>>> List of written blog articles <<<<<<')
-          this.displayAllWrittenBlogArticles = response.data
-        })
-    }
+    ...mapActions({
+      fetchListOfWrittenArticles: 'blog/fetchListOfWrittenArticles'
+    })
   }
 }
 </script>
